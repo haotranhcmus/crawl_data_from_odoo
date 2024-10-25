c00350e494641d7da10854b83a1dc6a6824ddc8b
Hao Nhut Tran <haotranhcmus@gmail.com>
2024-10-25 18:42:02 +0700

    Update clear.py

--- clear.py
+++ clear.py
@@ -2,3 +2,4 @@ with open("data.txt", "w", encoding="utf-8") as file:
         file.write("|{:^65}|{:^65}|{:^65}|\n".format('Module', 'Question', 'Answer').replace(' ', '_'))
 with open("data.csv", "w", encoding="utf-8") as file:
         pass
+print("Done")
